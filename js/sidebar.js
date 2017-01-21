4056a2e856294a9169719a3230b11bda518c4345
Angel De La Cruz <angeldelacruzdev@gmail.com>
2017-01-21 18:00:44 -0400

    Fisrt commit -*-

---
+++ js/sidebar.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function () {
+   
+
+
+    $(window).on('resize', function () {
+        console.log($(window).width());
+        if ($(window).width() >= 992) {
+            $('.collapse').collapse('show');
+             return false;
+        }
+        if ($(window).width() <= 992) {
+           $('.collapse').collapse('hide');
+           return false;
+        }
+    });
+
+
+});
